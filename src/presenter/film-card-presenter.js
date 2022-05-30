27442c4c96e2334449defc5fa4ad383c7eb981b9
dimpulsha <dimpulsha@gmail.com>
2022-05-30 20:38:24 +0300

    правка названия ф-ции

--- src/presenter/film-card-presenter.js
+++ src/presenter/film-card-presenter.js
@@ -83,7 +83,7 @@ export default class FilmCardPresenter  {
     this.#popupRemove();
   };
 
-   #handleFilmCardClick = () => {
+  #handleFilmCardClick = () => {
     this.#showPopup();
     this.#rootPage.setEscKeyDownHandler(this.#handleEscKeyDown);
   };
