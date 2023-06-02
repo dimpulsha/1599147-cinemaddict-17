6c2911ddeb06bb004c5d61aec7b50541f3012ba5
dimpulsha <dimpulsha@gmail.com>
2023-06-02 12:54:40 +0500

    базовая конфигурация сборки

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
 const path = require('path');
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'build'),
     clean: true,
-  },
+   },
   devtool: 'source-map',
   plugins: [
+    new HtmlPlugin({
+      template: 'public/index.html',
+    }),
     new CopyPlugin({
-      patterns: [{ from: 'public' }],
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+        },
+      ],
     }),
   ],
   module: {
@@ -19,12 +30,17 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
-        use: ['babel-loader']
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
       },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
-      }
+      },
     ]
   }
 };
